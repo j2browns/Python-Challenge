948d3deea435fa57ce79a9ed69178a79fa6d360c
unknown <j2browns@yahoo.com>
2020-06-30 22:52:28 -0500

    rationalized dictionary so candidate key with votes and percent

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,33 +14,42 @@ with open(PyPoll_csv,'r') as csvfile:
     PyPoll_header = next(csvfile) #reading header from file
     
     #checking Header is correct by printing.  To be removed or commented out in final version
-    print(PyPoll_header)
+    #print(PyPoll_header)
 
-#reading remainder of data set in csv into Monthly_data
-#    Poll_data = list(csvreader)
+#reading remainder of data set in csv then getting candidates and totalling votes
 
-    Candidate_List = []
-    Candidate_Votes =[]
+    Candidate_List = []#will be list of candidates
+    Candidate_Votes =[]#will be list of votes for candidate
     for row in csvreader:
-        Candidate = (row[2])
-        if Candidate not in Candidate_List:
-             Candidate_List.append(Candidate)
-             Candidate_Votes.append(0)
+        Candidate = (row[2])#getting candidate name
+        if Candidate not in Candidate_List: #if candidate is not in list then add
+             Candidate_List.append(Candidate) #add candidate
+             Candidate_Votes.append(0) #add spot in votes
         for i in range (0,len(Candidate_List)):
-            if Candidate == Candidate_List[i]:
-                Candidate_Votes[i] +=1
+            if Candidate == Candidate_List[i]: #finding index of candidate
+                Candidate_Votes[i] +=1 #adding to counts for candidate
 
 Total_Votes = 0
 Percent_Votes = 0
 Total_Votes = sum(Candidate_Votes)
-Percent_Votes = [round(float(Candidate_Votes[i]/Total_Votes*100),3) for i in range(0,len(Candidate_Votes))]
-#Percent_Votes = Candidate_Votes/Total_Votes*100 
-Summary = { "Candidates:": Candidate_List,
-            "Votes":Candidate_Votes
-            }
-print(Summary)
-#print(Candidate_List)
-#print(Candidate_Votes)
-#print(Total_Votes)
-#print(Percent_Votes)
+Percent_Votes = [(Candidate_Votes[i]/Total_Votes*100) for i in range(0,len(Candidate_Votes))]
+Percent_Votes = [round(Percent_Votes[i],2) for i in range(0,len(Candidate_Votes))]
+# using dictionary comprehension 
+# to convert lists to dictionary 
+Summary = {Candidate_List[i]: [Candidate_Votes[i], Percent_Votes[i]] for i in range(len(Candidate_List))} 
+Winner = max(Summary, key=Summary.get)
+# Printing resultant dictionary 
+
+#outputting to screen
+print("\n")
+print(f"Election Results\n")
+print(f"*************************")
+print(f"Total Votes: {Total_Votes}")
+for key , value in Summary.items(): #printing out candidate, votes and percent votes
+    print(f"Candidate: {key},  Votes: {value[0]},  Percent: {value[1]}%")
+
+print(f"*************************")
+print(f"Winner of election is: {Winner}\n")
+
+
 
